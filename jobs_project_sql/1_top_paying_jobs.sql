de20888315303a9904fcf2d54cef26613304815e
Christain Pugh <alexander.pugh95@gmail.com>
2024-08-08 13:32:38 -0400

    Added some insight write-ups for queries.

--- jobs_project_sql/1_top_paying_jobs.sql
+++ jobs_project_sql/1_top_paying_jobs.sql
@@ -6,6 +6,8 @@ QUESTION TO BE ANSWERED WITH THIS QUERY: What are the top paying data analyst jo
 offering insights into employment options and location flexibility
 */
 
+-- Georgia Data Analyst jobs query
+
 SELECT
     job_id,
     name AS company_name,
@@ -26,6 +28,19 @@ ORDER BY
     salary_year_avg DESC
 LIMIT 10;
 
+
+/*
+In the Georgia Data Analyst jobs query 
+The dataset showcases the top 10 job opportunities with a high concentration in Atlanta. 
+The average salary for the top 10 jobs is $168,449.
+with salaries ranging between $145,500 and $245,000. 
+Positions such as Data Analysts and Lead Analysts are most common. 
+The job postings span a 7-month period, 
+indicating ongoing demand for data professionals in the region.
+*/
+
+-- Remote Data Analyst jobs query
+
 SELECT
     job_id,
     name AS company_name,
@@ -45,3 +60,207 @@ WHERE
 ORDER BY
     salary_year_avg DESC
 LIMIT 10;
+
+/*
+In the Remote Data Analyst jobs Query
+The dataset presents a high demand for data professionals with 
+significant variations in salary reflecting different levels of seniority and specialization. 
+The average salary is approximately $291,009 with a median salary of $217,000. 
+The roles include a mix of senior positions such as Directors, and Principal Data Analysts, 
+as well as more specialized roles like ERM Data Analysts and Data Analysts in niche fields.
+The job postings are spread throughout 2023, indicating consistent hiring activity for data roles. 
+The data suggests that remote and hybrid work options are prevalent, 
+aligning with the broader trend towards flexible work arrangements.
+*/
+
+/*
+JSON FILE'S
+
+Georgia Jobs
+[
+  {
+    "job_id": 1147498,
+    "company_name": "Channel Personnel Services",
+    "job_title": "Director Analytics",
+    "job_location": "Atlanta, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "245000.0",
+    "job_posted_date": "2023-06-30 19:56:28"
+  },
+  {
+    "job_id": 87862,
+    "company_name": "Brown & Brown, Inc.",
+    "job_title": "Analytics and R&D Actuary",
+    "job_location": "Sandy Springs, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "170000.0",
+    "job_posted_date": "2023-06-11 07:04:10"
+  },
+  {
+    "job_id": 729613,
+    "company_name": "AT&T",
+    "job_title": "Principal-Data Analysis",
+    "job_location": "Atlanta, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "156500.0",
+    "job_posted_date": "2023-05-05 15:19:33"
+  },
+  {
+    "job_id": 1531603,
+    "company_name": "Citi",
+    "job_title": "Data Analytics Lead Analyst - Hybrid",
+    "job_location": "Atlanta, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "151950.0",
+    "job_posted_date": "2023-08-17 04:14:37"
+  },
+  {
+    "job_id": 1505349,
+    "company_name": "Citi",
+    "job_title": "Data Analytics - Lead Analyst (Hybrid)",
+    "job_location": "Atlanta, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "151950.0",
+    "job_posted_date": "2023-09-02 20:19:48"
+  },
+  {
+    "job_id": 1066248,
+    "company_name": "Citi",
+    "job_title": "Operational Risk and Control Data Analyst (Hybrid)",
+    "job_location": "Atlanta, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "150890.0",
+    "job_posted_date": "2023-04-12 00:52:19"
+  },
+  {
+    "job_id": 1104704,
+    "company_name": "Intuit Inc",
+    "job_title": "Staff Business Data Analyst",
+    "job_location": "Atlanta, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "150000.0",
+    "job_posted_date": "2023-10-04 07:41:19"
+  },
+  {
+    "job_id": 783006,
+    "company_name": "Staff Smart, Inc.",
+    "job_title": "Data Analyst",
+    "job_location": "Augusta, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "150000.0",
+    "job_posted_date": "2023-07-31 18:21:58"
+  },
+  {
+    "job_id": 1109465,
+    "company_name": "Affinity",
+    "job_title": "Data Analyst",
+    "job_location": "Augusta, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "150000.0",
+    "job_posted_date": "2023-07-27 00:05:46"
+  },
+  {
+    "job_id": 985699,
+    "company_name": "Peraton",
+    "job_title": "Data Analyst/ Engineer",
+    "job_location": "Gordon, GA",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "145500.0",
+    "job_posted_date": "2023-11-21 09:40:27"
+  }
+]
+
+Remote jobs
+[
+  {
+    "job_id": 226942,
+    "company_name": "Mantys",
+    "job_title": "Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "650000.0",
+    "job_posted_date": "2023-02-20 15:13:33"
+  },
+  {
+    "job_id": 547382,
+    "company_name": "Meta",
+    "job_title": "Director of Analytics",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "336500.0",
+    "job_posted_date": "2023-08-23 12:04:42"
+  },
+  {
+    "job_id": 552322,
+    "company_name": "AT&T",
+    "job_title": "Associate Director- Data Insights",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "255829.5",
+    "job_posted_date": "2023-06-18 16:03:12"
+  },
+  {
+    "job_id": 99305,
+    "company_name": "Pinterest Job Advertisements",
+    "job_title": "Data Analyst, Marketing",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "232423.0",
+    "job_posted_date": "2023-12-05 20:00:40"
+  },
+  {
+    "job_id": 1021647,
+    "company_name": "Uclahealthcareers",
+    "job_title": "Data Analyst (Hybrid/Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "217000.0",
+    "job_posted_date": "2023-01-17 00:17:23"
+  },
+  {
+    "job_id": 168310,
+    "company_name": "SmartAsset",
+    "job_title": "Principal Data Analyst (Remote)",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "205000.0",
+    "job_posted_date": "2023-08-09 11:00:01"
+  },
+  {
+    "job_id": 731368,
+    "company_name": "Inclusively",
+    "job_title": "Director, Data Analyst - HYBRID",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189309.0",
+    "job_posted_date": "2023-12-07 15:00:13"
+  },
+  {
+    "job_id": 310660,
+    "company_name": "Motional",
+    "job_title": "Principal Data Analyst, AV Performance Analysis",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "189000.0",
+    "job_posted_date": "2023-01-05 00:00:25"
+  },
+  {
+    "job_id": 1749593,
+    "company_name": "SmartAsset",
+    "job_title": "Principal Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "186000.0",
+    "job_posted_date": "2023-07-11 16:00:05"
+  },
+  {
+    "job_id": 387860,
+    "company_name": "Get It Recruit - Information Technology",
+    "job_title": "ERM Data Analyst",
+    "job_location": "Anywhere",
+    "job_schedule_type": "Full-time",
+    "salary_year_avg": "184000.0",
+    "job_posted_date": "2023-06-09 08:01:04"
+  }
+]
+*/
--- jobs_project_sql/3_top_demanded_skills.sql
+++ jobs_project_sql/3_top_demanded_skills.sql
@@ -7,6 +7,8 @@ QUESTION: What are the most in-demand skills for data analysts?
     providing insights into the most valuable skills for job seekers
 */
 
+
+-- Remote Data Analyst skills demand
 SELECT
     skills,
     COUNT(skills_job_dim.job_id) AS demand_count
@@ -22,6 +24,20 @@ ORDER BY
     demand_count DESC
 LIMIT 5;
 
+/*
+In the Remote skills demand query we find the two highest in demand skills
+to be SQL and Excel, both data table softwares with SQL having a larger capacity
+for large data sets and its own coding language used to specify results.
+In the realm of coding languages Python seems to win over R for the top 5 most demanded slots
+This is likely due to its incredible flexibility when it somes to big data,
+data scraping, and sorting through data with a vast array of libraries at the users disposal.
+Finally taking 4th, and 5th place we have Tableau, and Power BI.
+Data visualization tools commonly used in the data field in order to help
+data analysts break down data in a much more readable and presentable fashion.
+*/
+
+
+-- Georgia Data Analyst skills demand
 SELECT
     skills,
     COUNT(skills_job_dim.job_id) AS demand_count
@@ -36,3 +52,64 @@ GROUP BY
 ORDER BY 
     demand_count DESC
 LIMIT 5;
+
+/*
+In the Georgia Data Analyst skills demand we only see a small variation
+from the remote skills demand while SQL and Excel remain as the top 2 skills
+of a Data Analyst in Georgia for 2023, Python has moved down one placement, remaining
+still the highest in demand coding language for Data Analysts with tableau taking 3rd
+and SAS making its way into the top 5. SAS's demand remains high due to its
+advanced data analytical tools as well as having its own data visualization tool
+known as SAS visual analytics
+*/
+
+/*
+JSON FILES FOR QUERIES
+Remote Query JSON file
+[
+  {
+    "skills": "sql",
+    "demand_count": "7291"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "4611"
+  },
+  {
+    "skills": "python",
+    "demand_count": "4330"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "3745"
+  },
+  {
+    "skills": "power bi",
+    "demand_count": "2609"
+  }
+]
+
+GA Query JSON file
+[
+  {
+    "skills": "sql",
+    "demand_count": "2318"
+  },
+  {
+    "skills": "excel",
+    "demand_count": "1848"
+  },
+  {
+    "skills": "tableau",
+    "demand_count": "1242"
+  },
+  {
+    "skills": "python",
+    "demand_count": "1089"
+  },
+  {
+    "skills": "sas",
+    "demand_count": "936"
+  }
+]
+*/
